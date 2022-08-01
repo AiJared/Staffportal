7d5d9102e0a56f5265421a1f377b79abdb0a05e8
Kerriea-star <ojared511@gmail.com>
2022-08-01 12:36:25 +0300

    permissions file

---
+++ accounts/permissions.py
@@ -0,0 +1,27 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+class ISWorker(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+
+    def has_object_permissions(self, request, view, obj):
+        if request.user.role == "worker":
+            return True
+        if request.method is SAFE_METHODS:
+            return True
+        return False
+
+class IsAdminidtrator(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        
+    def has_object_permission(self, request, view, obj):
+        if (request.user.role == "administrator" or
+            request.user.is_admin or 
+            request.user.is_staff):
+            return True
+        if request.method in SAFE_METHODS:
+            return True
+        return False
