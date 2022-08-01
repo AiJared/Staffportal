f09e9ff5ec6cb447b10917c39be2adc1fc45f5e0
Kerriea-star <ojared511@gmail.com>
2022-08-01 14:29:35 +0300

    admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from accounts.models import Administrator, Worker, User
 
-# Register your models here.
+admin.site.unregister(Group)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['email', 'username']
+    list_display = ("username", "email", "role",
+                    "phone", "is_active", "is_admin",
+                    "is_staff", "timestamp")
+    list_filter = ("is_active", "is_admin", "is_staff", "role")
+
+
+@admin.register(Administrator)
+class AdministratorAdmin(admin.ModelAdmin):
+    search_fields = ['get_username',]
+    list_display = ("get_username", "first_name", "last_name", "county", "town", "estate")
+
+    def get_username(self, obj):
+        return obj.user.username
+    get_username.short_description = "Username"
+    get_username.admin_order_field = "user__username"
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    search_fields = ["get_username",]
+    list_display = ("get_username", "first_name", "last_name",
+                    "company", "department", 
+                    "year", "county", "town", "address",
+                    "postal_code")
+    
+    def get_username(self, obj):
+        return obj.user.username
+    get_username.short_description = "Username"
+    get_username.admin_order_field = "user__username"
